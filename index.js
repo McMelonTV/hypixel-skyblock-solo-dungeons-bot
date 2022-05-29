fae14725ee6abb70699ca73459a6451ca2c4053b
McMelonTV <62901799+McMelonTV@users.noreply.github.com>
2022-05-29 19:16:49 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const mineflayer = require("mineflayer")
+const config = require("./config.json")
+const fetch = require("node-fetch")
+var moment = require('moment-timezone')
+
+const bot = mineflayer.createBot({
+    host: 'mc.hypixel.net',
+    port: 25565,
+    username: config.minecraftemail,
+    password: config.minecraftpassword
+  })
+
+bot.on("login", async => {
+    console.log("Minecraft > Logged in")
+})
+
+bot.on('message', msg => {
+	  var allowedUsers = ['McMelonTV', 'user1', 'user2', 'etc']
+    console.log("[" + moment().tz('Europe/London').format("HH:mm:ss") + "] " + msg.toString())
+    if (allowedUsers.some(usr => msg.toString().includes(usr + " has invited you to join their party!"))) {
+        bot.chat(`/p accept ${msg.toString().replace(/-/g,"").replace("You have 60 seconds to accept. Click here to join!","").replace(" has invited you to join their party!","").replace(/(\r\n|\n|\r)/gm,"")}`)
+        setTimeout(function(){bot.chat(`/pc You have 10 seconds to join a dungeon before I leave the party.`)}, 500)
+        
+   		setTimeout(leaveParty, 12000)
+
+    	function leaveParty() {
+        	bot.chat(`/p leave`)
+        	setTimeout(function(){bot.chat(`/l`)}, 500)
+    	}
+    } else if (allowedUsers.some(usr => msg.toString().includes(usr + " entered The Catacombs"))) {
+        bot.chat(`/p leave`)
+    }
+})
