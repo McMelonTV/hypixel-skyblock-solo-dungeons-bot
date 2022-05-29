b930c9598ae2facff02101180e1f759db9d4a2d4
McMelonTV <62901799+McMelonTV@users.noreply.github.com>
2022-05-29 19:38:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const mineflayer = require("mineflayer")
 const { email, password, account_type } = require("./config.json")
-var moment = require('moment-timezone')
+var moment = require("moment-timezone")
 
 const bot = mineflayer.createBot({
-    host: 'mc.hypixel.net',
+    host: "mc.hypixel.net",
     port: 25565,
     version: "1.8.9",
     username: email,
@@ -16,19 +16,20 @@ bot.on("login", async => {
 })
 
 bot.on('message', msg => {
-	  var allowedUsers = ['McMelonTV', 'user1', 'user2', 'etc']
-    console.log("[" + moment().tz('Europe/London').format("HH:mm:ss") + "] " + msg.toString())
-    if (allowedUsers.some(usr => msg.toString().includes(usr + " has invited you to join their party!"))) {
-        bot.chat(`/p accept ${msg.toString().replace(/-/g,"").replace("You have 60 seconds to accept. Click here to join!","").replace(" has invited you to join their party!","").replace(/(\r\n|\n|\r)/gm,"")}`)
-        setTimeout(function(){bot.chat(`/pc You have 10 seconds to join a dungeon before I leave the party.`)}, 500)
+	var allowedUsers = require("./config.json")
+    	console.log("[" + moment().tz('Europe/London').format("HH:mm:ss") + "] " + msg.toString())
+    	if (allowedUsers.some(usr => msg.toString().includes(usr + " has invited you to join their party!"))) {
+        	bot.chat(`/p accept ${msg.toString().replace(/-/g,"").replace("You have 60 seconds to accept. Click here to join!","").replace(" has invited you to join their party!","").replace(/(\r\n|\n|\r)/gm,"")}`)
+        	setTimeout(function(){bot.chat("/pc You have 10 seconds to join a dungeon before I leave the party.")}, 500)
         
    		setTimeout(leaveParty, 12000)
 
-    	function leaveParty() {
-        	bot.chat(`/p leave`)
-        	setTimeout(function(){bot.chat(`/l`)}, 500)
+    		function leaveParty() {
+        		bot.chat("/p leave")
+        		setTimeout(function(){bot.chat("/l")}, 500)
+    		}
+    	} else if (allowedUsers.some(usr => msg.toString().includes(usr + " entered The Catacombs"))) {
+        	bot.chat("/p leave")
+        	setTimeout(function(){bot.chat("/l")}, 500)
     	}
-    } else if (allowedUsers.some(usr => msg.toString().includes(usr + " entered The Catacombs"))) {
-        bot.chat(`/p leave`)
-    }
 })
